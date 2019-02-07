ede9e9b76cf71625027ff95db0f061a7925eb1c5
LenaSava <34009219+LenaSava@users.noreply.github.com>
2019-02-07 21:53:00 +0200

    Update View.java

--- src/View.java
+++ src/View.java
@@ -1,10 +1,9 @@
 public class View {
-    // Text's constants
+
     public static String SayHiToTheWorld = "Say hi to the world: ";
     public static String TryAgain = "Be cool, try again: ";
     public static String FinalInput = "You said: ";
 
-    //replace sout
     public void printMessage (String massage){
         System.out.println(massage);
     }
